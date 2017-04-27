f2dc26dceee56c6c1def71fe96a680c55c6efe21
justin38 <justinya07@gmail.com>
2017-04-27 13:44:30 -0700

    add

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,10 +1,10 @@
 ### Exercise 2 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
-
+library("httr")
+library("jsonlite")
 # Create a `base.uri` variable that holds the base uri. You wil then paste endpoints to this base.
-
+base.url <- "https://api.spotify.com"
 
 
 ## As you may have noticed, it often takes multiple queries to retrieve the desired information.
@@ -14,18 +14,33 @@
 
 # Define a function `TopTrackSearch` that takes in an artist name as an argument,
 # and returns the top 10 tracks (in the US) by that artist
-
+TopTrackSearch <- function(name){
+  resource <- "/v1/search"
+  url = paste0(base.url, resource)
+  querys <- list(q = name, type = "artist")
+  response <- GET(uri,query = querys)
+  artist.info <- fromJSON(content(response, "text"))
+  artist.id <- artist.info$artists$items$id[1]
+  
+  resource <- paste0("/v1/artists/", artist.id, "/top-tracks")
+  uri = paste0(base.uri, resource)
+  queryss <- list(country = "US")
+  response <- GET(uri, query = queryss)
+  
+  top.tracks <- fromJSON(content(response, "text"))
+  return(top.tracks$tracks)
+  }
 
 
 
 # What are the top 10 tracks by Beyonce?
-
+Beyonce <- TopTrackSearch('Beyonce')
 
 # Use the `flatten` function to flatten the data.frame -- note what differs!
-
+flattens <- flatten(top.tracks)
 
 # Use the `save()` function to save the flattened data frame to a file `beyonce.Rdata`
-
+save(Beyonce, file="beyonce.Rdata")
 
 # Use your `dplyr` functions to get the number of the songs that appear on each album
 
